4f11a9d1b331e35b9a6fd9e180849ce6b9f4a36f
xiaohe123awa <115407956+xiaohe123awa@users.noreply.github.com>
2024-10-15 02:40:22 +0800

    Update and rename main 3.py to main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
+# Author: @xiaohe123awa
 import requests
 import time
 import json
 
-# 人气popularity
-# 点赞total_upvote_num
 url = "https://u5pyq.webapp.163.com/apps/u5/house/get_house_data?avatar_id=ZDk%2BNgYrcxAaCF4K"
 headers = {
   "Host": "u5pyq.webapp.163.com",
   "Connection": "keep-alive",
   "content-type": "application/json",
-  "Accept-Encoding": "gzip,compress,br,deflate",
-  "User-Agent": "Mozilla/5.0 (iPad; CPU OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x18003137) NetType/WIFI Language/zh_CN",
-  "Referer": "http"
+  "Accept-Encoding": "gzip,compress,br,deflate"
 }
 response = requests.get(url, headers=headers).json()
 response = response['data']['house']
-print(response)
 old_popularity = json.loads(response)['popularity']
 old_total_upvote_num = json.loads(response)['total_upvote_num']
 print('初始化完成...')
@@ -26,7 +22,7 @@ while True:
         "Connection": "keep-alive",
         "content-type": "application/json",
         "Accept-Encoding": "gzip,compress,br,deflate",
-        "User-Agent": "Mozilla/5.0 (iPad; CPU OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x18003137) NetType/WIFI Language/zh_CN",
+        "User-Agent": "Mozilla/5.0 (iPone; CPU OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x18003137) NetType/WIFI Language/zh_CN",
         "Referer": "http"
     }
 
@@ -37,9 +33,9 @@ while True:
     if popularity != old_popularity or total_upvote_num != old_total_upvote_num:
         new_popularity = popularity
         new_total_upvote_num = total_upvote_num
-        requests.post('http://localhost:3113/send_group_msg', json={
-            'user_id': 980811231,
-            'group_id': 980811231,
+        requests.post('http://localhost:3000/send_group_msg', json={
+            'user_id': 群号,
+            'group_id': 群号,
             'message': [{
                 'type': 'text',
                 'data': {
